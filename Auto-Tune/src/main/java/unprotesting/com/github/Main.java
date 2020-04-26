993826201c5c3314a3f0aeb976e08c72a6f87c51
Sirzuki <nuclarkson@gmail.com>
2020-04-26 16:23:02 +0100

    Added Vault Plugin Hook
    + Added Vault Plugin Hook
    + Fixed Error in Lodaing
    + Setup Test-Command
    + Removed Unnecessary import and functions.
    + Updated to 0.1.1

--- Auto-Tune/src/main/java/unprotesting/com/github/Main.java
+++ Auto-Tune/src/main/java/unprotesting/com/github/Main.java
@@ -1,31 +1,43 @@
 package unprotesting.com.github;
 
-import net.milkbowl.vault.chat.Chat;
+import java.util.logging.Logger;
 import net.milkbowl.vault.economy.Economy;
-import java.security.Permissions;
-import org.bukkit.Bukkit;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-    private Economy econ;
-    private Permissions perms;
-    private Chat chat;
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static Economy econ;
+
+    FileConfiguration config;
+    File cfile;
 
     @Override
-    public void onEnable(){
-        if (!setupEconomy()) {
-            this.getLogger().severe("Disabled due to no Vault dependency found!");
+    public void onDisable() {
+        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
+    }
+
+    @Override
+    public void onEnable() {
+        if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        this.setupPermissions();
-        this.setupChat();
     }
-
+    
     private boolean setupEconomy() {
-        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
         }
 
@@ -36,30 +48,35 @@ public class Main extends JavaPlugin {
         econ = rsp.getProvider();
         return econ != null;
     }
+    
 
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
-        return chat != null;
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if(!(sender instanceof Player)) {
+            log.info("Only players are supported for this Example Plugin, but you should not do this!!!");
+            return true;
+        }
+        
+        OfflinePlayer player = (Player) sender;
+        
+        if(command.getLabel().equals("test-economy")) {
+            // Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
+            sender.sendMessage(String.format("You have %s", econ.format(econ.getBalance(player))));
+            EconomyResponse r = econ.depositPlayer(player, 1.05);
+            if(r.transactionSuccess()) {
+                sender.sendMessage(String.format("You were given %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
+            } else {
+                sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
+            }
+            return true;
+        }   else {
+                return false;
+        }
     }
-
-    private boolean setupPermissions() {
-        RegisteredServiceProvider<Permissions> rsp = getServer().getServicesManager().getRegistration(Permissions.class);
-        perms = rsp.getProvider();
-        return perms != null;
-    }
-
-    public Economy getEconomy() {
+    
+    public static Economy getEconomy() {
         return econ;
     }
-
-    public Permissions getPermissions() {
-        return perms;
-    }
-
-    public Chat getChat() {
-        return chat;
-    }
+    
+    
 
 }
- 

6bbabe1584bb9b9fb882a3bd9ad76cc14c8d9d56
Noah Clarkson (Unprotesting) <unprotesting.email@gmail.com>
2021-11-22 18:31:10 +0000

    Recreate LocalServer and StaticFileHander under different folder
    Recreate LocalServer and StaticFileHander under different folder

---
+++ src/main/java/unprotesting/com/github/localserver/LocalServer.java
@@ -0,0 +1,33 @@
+package unprotesting.com.github.localserver;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+import unprotesting.com.github.config.Config;
+import unprotesting.com.github.logging.Logging;
+
+//  Local static HTTP server for price-data etc.
+
+public class LocalServer {
+
+    private HttpServer server;
+    private String base = "plugins/Auto-Tune/web";
+
+    public LocalServer() throws IOException{
+        if (Config.isWebServer()) {
+            try{
+                server = HttpServer.create(new InetSocketAddress(Config.getPort()), 0);
+                server.createContext("/", new StaticFileHandler(base));
+                server.setExecutor(null);
+                server.start();
+                Logging.debug("Web server has started on port " + Config.getPort());
+            }
+            catch(NullPointerException | IllegalArgumentException | IllegalStateException | IOException e){
+                Logging.error("Error Creating Server on port: " + Config.getPort() + ". Please try restarting or changing your port.");
+            }
+        }
+    }
+    
+}

16c51ac33e73fa49775445a128ca7665ddffd7b7
Dávid Malich <david.malich2@gmail.com>
2022-07-14 22:38:06 +0200

    Removed unused import

--- src/main/java/unprotesting/com/github/commands/SellCommand.java
+++ src/main/java/unprotesting/com/github/commands/SellCommand.java
@@ -4,7 +4,6 @@ import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;

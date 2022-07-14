49ac2217d81438b0835bacccac4e7e7747903504
Dávid Malich <david.malich2@gmail.com>
2022-07-14 22:34:03 +0200

    Optimized code Command setup rework Optimized event imports

--- src/main/java/unprotesting/com/github/AutoTune.java
+++ src/main/java/unprotesting/com/github/AutoTune.java
@@ -3,19 +3,16 @@ package unprotesting.com.github;
 import lombok.Getter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 import unprotesting.com.github.commands.AutosellCommand;
 import unprotesting.com.github.commands.LoanCommand;
 import unprotesting.com.github.commands.SellCommand;
 import unprotesting.com.github.commands.ShopCommand;
 import unprotesting.com.github.config.Config;
 import unprotesting.com.github.data.Database;
-import unprotesting.com.github.events.AutoTuneInventoryCheckEvent;
-import unprotesting.com.github.events.AutosellProfitEvent;
-import unprotesting.com.github.events.IpCheckEvent;
-import unprotesting.com.github.events.LoanInterestEvent;
-import unprotesting.com.github.events.TimePeriodEvent;
-import unprotesting.com.github.events.TutorialEvent;
+import unprotesting.com.github.events.*;
 import unprotesting.com.github.server.LocalServer;
 import unprotesting.com.github.util.EconomyUtil;
 
@@ -24,65 +21,72 @@ import unprotesting.com.github.util.EconomyUtil;
  */
 public class AutoTune extends JavaPlugin {
 
-  // The static instance of the plugin.
-  @Getter
-  private static AutoTune instance;
+    // The static instance of the plugin.
+    @Getter
+    private static AutoTune instance;
 
-  @Override
-  public void onEnable() {
-    instance = this;
-    EconomyUtil.setupLocalEconomy(Bukkit.getServer());
-    Config.init();
-    setupEvents();
-    new Database();
-    setupCommands();
-    new Metrics(this, 9687);
-    LocalServer.initialize();
-  }
+    @Override
+    public void onEnable() {
+        instance = this;
 
-  @Override
-  public void onDisable() {
-    Database.get().close();
-    getLogger().info("Auto-Tune is now disabled!");
-  }
+        EconomyUtil.setupLocalEconomy(Bukkit.getServer());
+        Config.init();
 
-  private void setupCommands() {
-    getCommand("shop").setExecutor(new ShopCommand());
-    getCommand("sell").setExecutor(new SellCommand());
-    getCommand("autosell").setExecutor(new AutosellCommand());
-    if (Config.get().isEnableLoans()) {
-      getCommand("loan").setExecutor(new LoanCommand());
+        setupEvents();
+        setupCommands();
+
+        new Database();
+        new Metrics(this, 9687);
+
+        LocalServer.initialize();
     }
-  }
 
-  private void setupEvents() {
-
-    Bukkit.getScheduler().runTaskAsynchronously(this,
-        () -> Bukkit.getPluginManager().callEvent(new IpCheckEvent(true)));
-
-    Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-        () -> Bukkit.getPluginManager().callEvent(new AutosellProfitEvent(true)),
-        1200L, 1200L);
-
-    Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-        () -> Bukkit.getPluginManager().callEvent(new TimePeriodEvent(true)),
-        (long) (Config.get().getTimePeriod() * 1200L),
-        (long) (Config.get().getTimePeriod() * 1200L));
-
-    Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-        () -> Bukkit.getPluginManager().callEvent(new TutorialEvent(true)),
-        (long) (Config.get().getTutorialUpdate() * 20),
-        (long) (Config.get().getTutorialUpdate() * 20));
-
-    Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-        () -> Bukkit.getPluginManager().callEvent(new AutoTuneInventoryCheckEvent(true)),
-        600L, 4L);
-
-    if (Config.get().isEnableLoans()) {
-      Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-          () -> Bukkit.getPluginManager().callEvent(new LoanInterestEvent(true)),
-          1200L, 1200L);
+    @Override
+    public void onDisable() {
+        Database.get().close();
+        getLogger().info("Auto-Tune is now disabled!");
     }
-  }
-  
+
+    private void setupCommands() {
+        new ShopCommand(this);
+        new SellCommand(this);
+        new AutosellCommand(this);
+        if (Config.get().isEnableLoans()) {
+            new LoanCommand(this);
+        }
+    }
+
+    private void setupEvents() {
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        Config config = Config.get();
+
+        scheduler.runTaskAsynchronously(this,
+                () -> pluginManager.callEvent(new IpCheckEvent(true)));
+
+        scheduler.runTaskTimerAsynchronously(this,
+                () -> pluginManager.callEvent(new AutosellProfitEvent(true)),
+                1200L, 1200L);
+
+        scheduler.runTaskTimerAsynchronously(this,
+                () -> pluginManager.callEvent(new TimePeriodEvent(true)),
+                (long) (config.getTimePeriod() * 1200L),
+                (long) (config.getTimePeriod() * 1200L));
+
+        scheduler.runTaskTimerAsynchronously(this,
+                () -> pluginManager.callEvent(new TutorialEvent(true)),
+                (long) (config.getTutorialUpdate() * 20),
+                (long) (config.getTutorialUpdate() * 20));
+
+        scheduler.runTaskTimerAsynchronously(this,
+                () -> pluginManager.callEvent(new AutoTuneInventoryCheckEvent(true)),
+                600L, 4L);
+
+        if (config.isEnableLoans()) {
+            scheduler.runTaskTimerAsynchronously(this,
+                    () -> pluginManager.callEvent(new LoanInterestEvent(true)),
+                    1200L, 1200L);
+        }
+    }
+
 }

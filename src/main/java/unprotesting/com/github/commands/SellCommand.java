10d24d3841236abd95497ef8fc64a3ad03971345
Shawn11564 <shawnmicheldev@gmail.com>
2022-09-16 19:21:29 -0500

    Make /sell gui accept stack sizes larger than the sell limit and automatically return excess items (#90)

--- src/main/java/unprotesting/com/github/commands/SellCommand.java
+++ src/main/java/unprotesting/com/github/commands/SellCommand.java
@@ -1,6 +1,7 @@
 package unprotesting.com.github.commands;
 
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,8 +9,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import unprotesting.com.github.AutoTune;
+import unprotesting.com.github.config.Config;
 import unprotesting.com.github.data.PurchaseUtil;
+import unprotesting.com.github.data.ShopUtil;
+import unprotesting.com.github.util.EconomyUtil;
 import unprotesting.com.github.util.Format;
+
+import java.util.HashMap;
 
 /**
  * The command for selling items.
@@ -34,12 +40,37 @@ public class SellCommand implements CommandExecutor {
 
     private boolean interpret(@NotNull Player player) {
         player.getOpenInventory().close();
+        Config config = Config.get();
+        boolean isSellLimits = config.isEnableSellLimits();
         ChestGui gui = new ChestGui(5, "Sell Panel");
         gui.setOnClose(event -> {
             for (ItemStack item : gui.getInventory().getContents()) {
                 if (item == null) {
                     continue;
                 }
+
+                if (isSellLimits) {
+                    int sellsLeft = ShopUtil.getSellsLeft(player, item.getType().toString().toLowerCase());
+                    int itemAmount = item.getAmount();
+                    int amountCantSell = itemAmount - sellsLeft;
+
+                    if (amountCantSell > 0) {
+                        // Handle player trying to sell more than they can
+                        ItemStack itemCantSell = item.clone();
+                        itemCantSell.setAmount(amountCantSell);
+
+                        HashMap<Integer, ItemStack> failed = player.getInventory().addItem(itemCantSell);
+                        if (!failed.isEmpty()) {
+                            player.getWorld().dropItem(player.getLocation(), failed.get(0));
+                        }
+
+                        TagResolver resolver = PurchaseUtil.getTagResolver(item.displayName(), 0, amountCantSell, EconomyUtil.getEconomy().getBalance(player), null);
+                        Format.sendMessage(player, config.getRunOutOfSells(), resolver);
+
+                        item.setAmount(sellsLeft);
+                    }
+                }
+
                 PurchaseUtil.sellItemStack(item, player);
             }
         });

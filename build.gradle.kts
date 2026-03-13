a29dce47579d72a7d926438b150347a5bddac023
Noah Clarkson <mrnoahclarkson@gmail.com>
2026-03-14 05:12:07 +1300

    fix: downgrade run-paper to v2.3.1 for proper Gradle 8 compatibility
    Versions 3.x seem to have unresolved variant issues on CI.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     pmd
     id("com.gradleup.shadow") version "9.3.1"
-    id("xyz.jpenilla.run-paper") version "3.0.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
     id("net.ltgt.errorprone") version "5.0.0"
 }
 
